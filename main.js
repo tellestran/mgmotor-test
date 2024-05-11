956b507d35634e1b7995817c614ba7b6c889f940
Tran Tien Ngoc <trantienngoc@192.168.1.7>
2024-05-11 09:37:48 +0700

    update desktop

--- main.js
+++ main.js
@@ -1,31 +1,45 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+  let slideIndex = 1;
+  showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
+  function plusSlides(n) {
+    showSlides((slideIndex += n));
+  }
 
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+  function currentSlide(n) {
+    showSlides((slideIndex = n));
+  }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("jumbotron__slide-element");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("jumbotron__slide-element");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
   }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
 
+  var accordions = document.querySelectorAll(
+    ".accordion, .faqs, .dropbtn-mobile"
+  );
+  accordions.forEach(function (accordion) {
+    accordion.addEventListener("click", function () {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  });
